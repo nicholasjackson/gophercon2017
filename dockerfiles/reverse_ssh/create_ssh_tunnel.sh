bde2199bb087db5bb8d0041f92c20efeb790d575
Nic Jackson <jackson.nic@gmail.com>
2017-01-08 11:02:54 -0500

    Added reverse ssh tunnel container

---
+++ dockerfiles/reverse_ssh/create_ssh_tunnel.sh
@@ -0,0 +1,26 @@
+#!/bin/bash 
+command="sshpass -p '${SERVER_PASSWORD}' \
+  ssh -o StrictHostKeyChecking=no \
+  -N -R 2222:localhost:22 \
+  ${SERVER_USER}@${SERVER_HOST}"
+
+createTunnel() { 
+  eval ${command}
+
+  if [[ $? -eq 0 ]]; then 
+    echo Tunnel to jumpbox created successfully 
+  else 
+    echo An error occurred creating a tunnel to jumpbox. RC was $? 
+  fi 
+} 
+
+for (( ; ; ))
+do
+  /bin/pidof ssh 
+  if [[ $? -ne 0 ]]; then 
+    echo Creating new tunnel connection
+    createTunnel
+  fi
+
+  sleep 60
+done
